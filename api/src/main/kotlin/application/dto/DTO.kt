3e86b9e68f009edbf49636c85a5cd914b9605124
Reni <103267773+ReniIrinyi@users.noreply.github.com>
2025-05-15 06:53:40 +0200

    DTO.kt aktualisieren

--- api/src/main/kotlin/application/dto/DTO.kt
+++ api/src/main/kotlin/application/dto/DTO.kt
@@ -2,12 +2,12 @@ package application.dto
 
 interface DTO <T> {
     /**
-     * Converts this DTO to the domain model (`ToiletModel`) for use in business logic or persistence.
+     * Converts this DTO to the domain model for use in business logic or persistence.
      */
     fun toModel():T
 
     /**
-     * Creates a `ToiletDTO` from a domain model (`ToiletModel`).
+     * Creates a DTO from a domain model
      * Used for preparing data for output (e.g., API responses).
      */
     companion object{}
